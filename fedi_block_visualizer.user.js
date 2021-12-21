936371648b2ca762c7fa98585e719973a2621493
nj-lc <grmnlc@pm.me>
2021-12-21 18:28:22 +0100

    added orange arrow for media removal

--- fedi_block_visualizer.user.js
+++ fedi_block_visualizer.user.js
@@ -3,7 +3,7 @@
 // @homepage    https://github.com/nj-lc/userscripts/
 // @match       https://youjo.love/*
 // @grant       none
-// @version     0.2
+// @version     0.3
 // ==/UserScript==
 
 let reject = [
@@ -42,11 +42,20 @@ let reject = [
     "mk.paritybit.ca"
 ]
 
+media_removal = [
+    "freecumextremist.com",
+    "leafposter.club",
+    "baraag.net"
+]
+
 
 setInterval(() => {
     Array.from(document.querySelectorAll(".Status")).map(status => {
         user = status.querySelector(".account-name")
         if (user.innerText.includes("@")) {
+            if (media_removal.includes(user.innerText.split("@")[1])) {
+                status.querySelector("button.button-unstyled.interactive").querySelector("path").style = "fill:orange;"
+            }
             if (reject.includes(user.innerText.split("@")[1])) {
                 status.querySelector("button.button-unstyled.interactive").querySelector("path").style = "fill:red;"
             }
