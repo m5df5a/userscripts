433771cfcc50145da45e0ac740f49411a4a7f65b
nj-lc <grmnlc@pm.me>
2021-12-21 14:14:08 +0100

    Added Pleroma Helper

---
+++ pleroma_helper.user.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @name        Pleroma Helper
+// @match       https://youjo.love/*
+// @grant       none
+// @version     0.1
+// ==/UserScript==
+
+let muted_words = []
+let remove_words = []
+
+setInterval(() => {
+    let post_box = document.querySelector(".main-input > textarea")
+
+    // change post placeholder
+    post_box.placeholder = "make a good post"
+  
+    // mute words
+    Array.from(document.querySelectorAll(".status-content")).map(element => {
+        if (element) {
+            muted_words.map(word => {
+                if (element.innerText.match(word)) {
+                    element.parentElement.parentElement.parentElement.parentElement.remove()
+                }
+            })
+        }
+    })
+
+    // remove words
+    Array.from(document.querySelectorAll(".status-content")).map(element => {
+        if (element) {
+            remove_words.map(word => {
+                element.innerHTML = element.innerHTML.replace(word, "")
+            })
+        }
+    })
+  
+    // Fix home instance button
+    let name = document.querySelector("#content > div.main > div > div > div.user-card.user-card-rounded-t > div.panel-heading > div > div.container > div > div.bottom-line > a")
+    let button = document.querySelector("#content > div.main > div > div > div.user-card.user-card-rounded-t > div.panel-heading > div > div.container > div > div.top-line > button")
+    if (name && button) {
+        name = name.innerText.split("@")
+        button.onclick = () => window.open("https://"+name[2]+"/"+name[1])
+    }
+  
+    // double spaces
+    Array.from(document.querySelectorAll(".status-content")).map(element => {
+        if (element) {
+            element.innerHTML = element.innerHTML.replace(/ {2,}/g, " ")
+        }
+    })
+    post_box.value = post_box.value.replace(/ {2,}/g, " ")
+}, 1000)
