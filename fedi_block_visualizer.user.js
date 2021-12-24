f24d65cfef7b7612dc87d983c77aeee5d0222f4c
nj-lc <grmnlc@pm.me>
2021-12-24 18:49:08 +0100

    changed whitespace

--- fedi_block_visualizer.user.js
+++ fedi_block_visualizer.user.js
@@ -7,61 +7,61 @@
 // ==/UserScript==
 
 let reject = [
-    "posting.lolicon.rocks",
-    "disqordia.space",
-    "waifuism.life",
-    "is.badat.dev",
-    "movsb.0x0.st",
-    "gamemaking.social",
-    "pl.gamers.exposed",
-    "mastodon.social",
-    "spectres.space",
-    "strelizia.net",
-    "sunbeam.city",
-    "coon.town",
-    "skaeya.com",
-    "cute.science",
-    "antabaka.me",
-    "misskey.bubbletea.dev",
-    "letsalllovela.in",
-    "mstdn.social",
-    "poa.st",
-    "octodon.social",
-    "bungle.online",
-    "sociale.network",
-    "mk.absturztau.be",
-    "neue.city",
-    "outerheaven.club",
-    "landofkittens.social",
-    "mk.catgirlsfor.science",
-    "fedi.absturztau.be",
-    "gearlandia.haus",
-    "shitposter.club",
-    "queer.hacktivis.me",
-    "varishangout.net",
-    "mk.paritybit.ca"
+  "posting.lolicon.rocks",
+  "disqordia.space",
+  "waifuism.life",
+  "is.badat.dev",
+  "movsb.0x0.st",
+  "gamemaking.social",
+  "pl.gamers.exposed",
+  "mastodon.social",
+  "spectres.space",
+  "strelizia.net",
+  "sunbeam.city",
+  "coon.town",
+  "skaeya.com",
+  "cute.science",
+  "antabaka.me",
+  "misskey.bubbletea.dev",
+  "letsalllovela.in",
+  "mstdn.social",
+  "poa.st",
+  "octodon.social",
+  "bungle.online",
+  "sociale.network",
+  "mk.absturztau.be",
+  "neue.city",
+  "outerheaven.club",
+  "landofkittens.social",
+  "mk.catgirlsfor.science",
+  "fedi.absturztau.be",
+  "gearlandia.haus",
+  "shitposter.club",
+  "queer.hacktivis.me",
+  "varishangout.net",
+  "mk.paritybit.ca"
 ];
 
 media_removal = [
-    "freecumextremist.com",
-    "leafposter.club",
-    "baraag.net"
+  "freecumextremist.com",
+  "leafposter.club",
+  "baraag.net"
 ];
 
 
 setInterval(() => {
-    "use strict";
-    Array.from(document.querySelectorAll(".Status")).map(status => {
-        let user = status.querySelector(".account-name");
-        if (user) {
-            if (user.innerText.includes("@")) {
-                if (media_removal.includes(user.innerText.split("@")[1])) {
-                    status.querySelector("button.button-unstyled.interactive path").style = "fill:orange;";
-                }
-                if (reject.includes(user.innerText.split("@")[1])) {
-                    status.querySelector("button.button-unstyled.interactive path").style = "fill:red;";
-                }
-            }
+  "use strict";
+  Array.from(document.querySelectorAll(".Status")).map(status => {
+    let user = status.querySelector(".account-name");
+    if (user) {
+      if (user.innerText.includes("@")) {
+        if (media_removal.includes(user.innerText.split("@")[1])) {
+          status.querySelector("button.button-unstyled.interactive path").style = "fill:orange;";
         }
-    })
+        if (reject.includes(user.innerText.split("@")[1])) {
+          status.querySelector("button.button-unstyled.interactive path").style = "fill:red;";
+        }
+      }
+    }
+  })
 }, 1000);
--- pleroma_helper.user.js
+++ pleroma_helper.user.js
@@ -8,41 +8,41 @@
 
 let muted_words = []; // array of strings or regex. posts that contain these will be removed.
 let replace_words = [
-    [/ {2,}/g, " "], // double spaces
-    [/( |<br>|^)(r\/.+?)( |<br>|$)/g, '<a href="https://www.reddit.com/$2">$1$2$3</a>'], // reddit
-    [/( |<br>|^)(\/[a-z349]{1,4}\/)( |<br>|$)/g, '<a href="https://boards.4channel.org$2">$1$2$3</a>'] // 4chan
+  [/ {2,}/g, " "], // double spaces
+  [/( |<br>|^)(r\/.+?)( |<br>|$)/g, '<a href="https://www.reddit.com/$2">$1$2$3</a>'], // reddit
+  [/( |<br>|^)(\/[a-z349]{1,4}\/)( |<br>|$)/g, '<a href="https://boards.4channel.org$2">$1$2$3</a>'] // 4chan
 ]; // array of arrays of 2 strings or regex. the first will be replaced with the second.
 
 setInterval(() => {
-    "use strict";
-    let post_box = document.querySelector(".main-input textarea");
+  "use strict";
+  let post_box = document.querySelector(".main-input textarea");
 
-    // change post placeholder
-    post_box.placeholder = localStorage.getItem("post_box_placeholder") || "Just landed in L.A.";
-  
-    // remove posts and words
-    Array.from(document.querySelectorAll(".Status")).map(status => {
-        if (status && !status.classList.contains("ph-replaced")) {
-            let content = status.querySelector(".status-content");
-            replace_words.map(words => {
-                content.innerHTML = content.innerHTML.replace(words[0], words[1]);
-            })
-            status.classList.add("ph-replaced");
-            muted_words.map(word => {
-                if (content.innerText.match(word)) {
-                    status.remove();
-                }
-            })
+  // change post placeholder
+  post_box.placeholder = localStorage.getItem("post_box_placeholder") || "Just landed in L.A.";
+
+  // remove posts and words
+  Array.from(document.querySelectorAll(".Status")).map(status => {
+    if (status && !status.classList.contains("ph-replaced")) {
+      let content = status.querySelector(".status-content");
+      replace_words.map(words => {
+        content.innerHTML = content.innerHTML.replace(words[0], words[1]);
+      })
+      status.classList.add("ph-replaced");
+      muted_words.map(word => {
+        if (content.innerText.match(word)) {
+          status.remove();
         }
-    })
-  
-    // Fix home instance button
-    let name = document.querySelectorAll(".user-summary")[1];
-    let button = document.querySelector(".external-link-button");
-    if (name && button) {
-        name = name.querySelector(".user-screen-name").innerText.split("@");
-        button.onclick = () => window.open("https://"+name[2]+"/"+name[1]);
+      })
     }
+  })
 
-    post_box.value = post_box.value.replace(/ {2,}/g, " ");
+  // Fix home instance button
+  let name = document.querySelectorAll(".user-summary")[1];
+  let button = document.querySelector(".external-link-button");
+  if (name && button) {
+    name = name.querySelector(".user-screen-name").innerText.split("@");
+    button.onclick = () => window.open("https://"+name[2]+"/"+name[1]);
+  }
+
+  post_box.value = post_box.value.replace(/ {2,}/g, " ");
 }, 1000);
